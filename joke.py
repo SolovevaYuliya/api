abafd5b8b2daff4816b10cfbfef6a1b0117ab67e
SolovevaYuliya <soloveva.iuliia89@yandex.ru>
2025-09-02 00:22:35 +0300

    Update joke.py

--- joke.py
+++ joke.py
@@ -1,8 +1,9 @@
 import requests
 
 url = "https://icanhazdadjoke.com/"
-headers = {"Accept": "application/json"}  # говорим серверу: "дай JSON"
+headers = {"Accept": "application/json"}  
 response = requests.get(url, headers=headers)
 data = response.json()
 
 print("Шутка:", data["joke"])
+
